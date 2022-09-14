9b47c05311e02404aae318212530c77a9288be00
solithachi <agbanuchisom.ng@gmail.com>
2022-09-14 13:56:02 -0700

    6-abs.c 7-print_last

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: n is an integer
+ * Description: prints the absolute value integer
+ * Return: int
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
