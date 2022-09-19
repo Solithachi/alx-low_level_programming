f7d85068c86c14863a3da34e78624e1934b110ba
solithachi <agbanuchisom.ng@gmail.com>
2022-09-19 09:19:34 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * reset_to_98 - takes an integer pointer and
+ * updates the value it points to to 98
+ * @n: an integer pointer that stores the address argument
+ * Return: returns nothing
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return 0;
 }
