2cb480b76194a68519ed66c75939fa012b0d7db4
solithachi <agbanuchisom.ng@gmail.com>
2022-09-22 02:02:39 -0700

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 if s1 and s2 are equal
+ * and returns 0 if they're equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+	{
+		f (s1[i] == 0)
+			return (0);
+	}
+	return (s1[i] - s2[i]);
+
+}
