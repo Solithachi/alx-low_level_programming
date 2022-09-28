4ed8b2ec9917e446112203e0eee762d7e793437e
solithachi <agbanuchisom.ng@gmail.com>
2022-09-27 20:47:13 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,7 +9,6 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
@@ -19,4 +18,3 @@ void print_chessboard(char (*a)[8])
 		_putchar('\n');
 	}
 }
-
