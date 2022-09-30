6f40c5d750208f299689ecd48ef3be91c6736590
solithachi <agbanuchisom.ng@gmail.com>
2022-09-29 21:36:02 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,19 @@
 
 /**
  * _puts_recursion - print a string
- * @s: the string to be printed
- * Return: returns nothing
+ * @s: pointer to char
+ * Return: No
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar (*s);
-		_puts_recursion((s + 1));
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar ('\n');
-	return
+	else
+	{
+		_putchar('\n');
+	}
 }
